379d355da04eba4c0c535d96bf820c996a8a4518
Iajuddin <iaja7898@gmail.com>
2025-06-01 20:47:18 +0600

    add square code

--- patterns/square.cpp
+++ patterns/square.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num=1;
+    for (int i=0; i < 3; i++){
+        for(int j=0; j< 3;j++){
+            cout<<num++<<" ";
+            
+        }
+        cout<<endl;
+    }
+    return 0;
 
+}
